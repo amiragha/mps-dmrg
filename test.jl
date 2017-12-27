c9e7b95ad7141d27a1b6834771d91782cf85f230
Amir M-Aghaei <amir.maghaei@gmail.com>
2017-12-26 20:58:54 -0800

    better test file.

--- test.jl
+++ test.jl
@@ -3,17 +3,42 @@ import Tmp
 
 #srand(1911)
 
-# test ket states and MPS
-d, Lx = rand(2:4, 2)
-ketstate = rand(Complex128, d^Lx)
-mps = Tmp.MPS(Lx, d, ketstate)
-@test ketstate ≈ Tmp.mps2ketstate(mps)
-@test norm(ketstate)^2 ≈ Tmp.norm(mps)[1,1]
-@test ketstate ≈ Tmp.mps2ketstate(mps)
+@testset "stuff" begin
 
-# test MPO measure
-H = Tmp.heisenberg(4)
-e, v = eigs(H, nev=1, which=:SR)
-mps = Tmp.MPS(4, 2, v[:,1])
-mpo = Tmp.MPO{Float64}(4, 2)
-@test Tmp.measure(mps, mpo) ≈ e
+    d, Lx = 2, 8
+
+    # A uniformly chosen random state as MPS
+    ketstate = complex.(randn(d^Lx), randn(d^Lx))
+    randmps = Tmp.MPS(Lx, d, ketstate)
+
+    # The Bethe chain ground state as MPS
+    H = Tmp.heisenberg(Lx)
+    eheis, vheis = eigs(H, nev=1, which=:SR)
+    mps = Tmp.MPS(Lx, 2, vheis[:,1])
+
+    @testset "ketstate" begin
+        @test norm(ketstate)^2 ≈ Tmp.norm(randmps)[1,1]
+        @test ketstate ≈ Tmp.mps2ketstate(randmps)
+    end
+
+    @testset "overlap" begin
+        @test Tmp.overlap(mps, mps) ≈ Tmp.norm(mps)
+        @test Tmp.overlap(mps, randmps) ≈ conj.(Tmp.overlap(randmps, mps))
+    end
+
+    @testset "measure" begin
+        mpo = Tmp.MPO{Float64}(Lx, 2)
+        @test Tmp.measure(mps, mpo) ≈ eheis
+    end
+
+    @testset "center" begin
+        # TODO:
+        # test canonicalize to right and left
+        # and center_at (how?)
+        Tmp.move_center!(randmps, Lx-1)
+        Tmp.move_center!(randmps, Lx-2)
+        Tmp.move_center!(randmps, 1)
+        Tmp.move_center!(mps, Lx)
+        @test Tmp.mps2ketstate(randmps) ≈ ketstate
+    end
+end
