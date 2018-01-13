62990959b2690a534b90ed32e83598214696cf9a
Amir M-Aghaei <amir.maghaei@gmail.com>
2018-01-12 18:17:44 -0800

    half-way through dmrg ...

--- src/DMRG.jl
+++ src/DMRG.jl
@@ -1,47 +1,46 @@
 """
-    dmrg_sweep(initial, mpo[, optimization])
+    dmrg_sweep_twosite(mps, mpo)
 """
-function dmrg_sweep(initial::MPS{T},
-                    mpo::MPO{T};
-                    optimization::Symbol=:twosite) where {T<:Union{Float64, Complex128}}
+function dmrg_sweep_twosite(initial::MPS{T},
+                            mpo::MPO{T}) where {T<:Union{Float64, Complex128}}
 
-    result = initial
-    # one full sweep
+    newmps = mps
 
-    # make the right tensor
+    Lx = mps.length
+    bond = 1
+    left  = ones(T, 1,1,1)
+    right = ones(T, 1,1,1)
+    mats = mps.matrices[site]
+    for site=Lx:-1:3
+        mats = mps.matrices[site]
+        mato = mpo.matrices[site]
 
-    # make the left tensor
+        @tensor right[lu,lm,ld] :=  conj(mats)[ld,rd,d'] *
+            (mato[d',lm,d,rm] * (mats[lu,ru,d] * right[ru,rm,rd]))
+    end
 
-    # find the new matrices
-    new_matrices = dmrg_lanczos(left_tensor, mp, right_tensor, input_vector)
+    ## QQQ? Is this the best way?!
+    function dmrg_matvec!(result::Vector{T},
+                          input::Vector{T})
+
+        reshape(input , diml, d, d, dimr)
+        reshape(result, diml, d, d, dimr)
+
+        @tensor result[ld, d1',d2',rd] := left[lu,lm,ld] * (
+            (mpo.matrices[bond][d1',lm,d1,mm] * mpo.matrices[bond+1][d2',mm,d2,rm]) *
+            (input[lu,d1,d2,ru] * right[ru,rm,rd]))
+
+        reshape(result, diml*d*d*dimr)
+        nothing
+    end
+
+    evals, evecs = (dmrg_matvec!, normalize(randn(diml*d*d*dimr)), true)
+
+    println(evals)
 
     # perform svd
 
     # push the new matrices
 
-    # do till end
-
     return result
-end
-
-"""
-    dmrg_matvec(left, mpo, right, v)
-
-This is matrix vector multiplication of the DMRG-MPS, we need to
-define this multiplication in a specific order for tensor contraction
-to get the best performance.
-
-"""
-function dmrg_matvec(left,
-                     mpo,
-                     right,
-                     v)
-
-end
-
-function dmrg_lanczos(lefttensor,
-                      mpo,
-                      righttensor,
-                      input_vector)
-
 end
